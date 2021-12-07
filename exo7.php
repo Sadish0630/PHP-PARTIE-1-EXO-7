cbe22de7ea26f225a50aa503fe4db3b9cb8f4a9f
audemar <audemarsadish@gmail.com>
2021-12-07 15:25:46 +0100

    exo7 terminé

---
+++ exo7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 7</title>
+</head>
+<body>
+    <p>
+   <?php
+   $lastname = 'Audemar';
+   $firstname = 'Sadish';
+   $age = 40;
+   echo 'Bonjour'.' '.$lastname. ' '.$firstname.', tu as'.' '.$age.'ans.'
+   ?>
+   </p>
+
+</body>
+</html>
